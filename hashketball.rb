efa3bc852bdb897adc274bdde7fa4a1f66fd2397
Sean Conrad <seanconrad123@gmail.com>
2018-06-24 23:31:18 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,7 @@
 def game_hash
   {:home => {
     :team_name => 'Brooklyn Nets',
-    :colors => ['black', 'white'],
+    :colors => ['Black', 'White'],
     :players => [
     {player_name: 'Alan Anderson', number: 0, shoe: 16, points: 22, rebounds: 12, assists: 12, steals: 3, blocks: 1, slam_dunks: 1},
     {player_name: 'Reggie Evans', number: 30, shoe: 14, points: 12, rebounds: 12, assists: 12, steals: 12, blocks: 12, slam_dunks: 7},
@@ -16,7 +16,7 @@ def game_hash
     :away => {
 
     :team_name => 'Charlotte Hornets',
-    :colors => ['turquoise', 'purple'],
+    :colors => ['Turquoise', 'Purple'],
     :players => [
     {player_name: 'Jeff Adrien', number: 4, shoe: 18, points: 10, rebounds: 1, assists: 1, steals: 2, blocks: 7, slam_dunks: 2},
     {player_name: 'Bismak Biyombo', number: 0, shoe: 16, points: 12, rebounds: 4, assists: 7, steals: 7, blocks: 15, slam_dunks: 10},
