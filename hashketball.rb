96522d374a403dd3f8adf1850a029f02aea05b52
Sean Conrad <seanconrad123@gmail.com>
2018-06-24 23:30:29 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -59,9 +59,9 @@ end
 
 def team_colors(name)
   if name == game_hash[:home][:team_name]
-    puts game_hash[:home][:colors]
+    print game_hash[:home][:colors]
   elsif name == game_hash[:away][:team_name]
-    puts game_hash[:away][:colors]
+    print game_hash[:away][:colors]
   end
 end
 
