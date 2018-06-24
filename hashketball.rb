3d1f6f8b2a4a6d7e486a161786f38478aacb77d3
Sean Conrad <seanconrad123@gmail.com>
2018-06-24 23:23:23 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -46,6 +46,8 @@ def shoe_size(player)
    game_hash[:home][:players].each do |x|
     if x[:player_name] == player
       puts x[:player_name][player]
+    else
+      game_hash[:home][:players].each do |x|
     end
   end
 end
