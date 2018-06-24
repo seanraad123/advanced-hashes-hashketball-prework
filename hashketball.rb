e87f298503e9ccc33622ecad1d4c96fe37a6427b
Sean Conrad <seanconrad123@gmail.com>
2018-06-24 23:14:27 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -29,7 +29,7 @@ def game_hash
 end
 
 def num_points_scored(player_name)
-  puts game_hash[:home][:players].each do |x|
+   game_hash[:home][:players].each do |x|
     print x
     #if a[:name] == player_name
   #    puts a[:points]
